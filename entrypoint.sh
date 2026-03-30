69c0f1f48f72c42ca6b0d0be539ebbeb1f0a7202
Hiran Wijesinghe <hiran.wijesinghe@diamond.ac.uk>
2026-03-30 18:30:38 +0000

    dynamically find nss wrapper

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "entrypoint.sh: running as uid=$(id -u) gid=$(id -g)" >&2
+
 USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 
@@ -9,7 +11,14 @@ EOF
 
 export NSS_WRAPPER_PASSWD=/tmp/passwd
 export NSS_WRAPPER_GROUP=/etc/group
-export LD_PRELOAD=libnss_wrapper.so
 export HOME=/home/user
+
+NSS_WRAPPER_LIB=$(ldconfig -p | grep libnss_wrapper | awk '{print $NF}' | head -1)
+if [[ -z "${NSS_WRAPPER_LIB}" ]]; then
+    echo "entrypoint.sh: WARNING libnss_wrapper not found" >&2
+else
+    echo "entrypoint.sh: using ${NSS_WRAPPER_LIB}" >&2
+    export LD_PRELOAD=${NSS_WRAPPER_LIB}
+fi
 
 exec "$@"
